a7249bcf1b9034e832174595f648552a043ff59d
okox <okoxxx@gmail.com>
2023-06-08 01:57:15 +0200

    Git ignore build

--- build/src/log.js
+++
@@ -1,9 +0,0 @@
-export function logMessage(message) {
-    if (message) {
-        console.log(`Log message: ${message}`);
-    }
-    else {
-        console.log('No message provided.');
-    }
-}
-//# sourceMappingURL=log.js.map
