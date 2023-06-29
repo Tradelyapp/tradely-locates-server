16488e2d26ca47b74d1cecea348893d089bcd8f1
okox <okoxxx@gmail.com>
2023-06-29 22:29:27 +0200

    Add ticker/shares/price to context. This context info is used for Cart historic

--- src/interfaces/context-data.interface.ts
+++ src/interfaces/context-data.interface.ts
@@ -1,7 +1,9 @@
 import {IMetroCallParameters} from "./metro-call-parameters.interface.js";
+import {ITransactionParameters} from "./transaction-parameters.interface.js";
 
 export interface IContextData {
     contextForPIN?: IMetroCallParameters;
     contextForShortConfirm?: IMetroCallParameters;
+    transactionParameters?: ITransactionParameters;
     timestamp: number;
 }
